9e5ac043aea11131617ad3f8a70427a29a83901a
GehanLiyanage2002 <iit22002.std.uwu.ac.lk>
2025-07-24 11:29:58 +0530

    fixed

--- api/login.php
+++ api/login.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once '../config/db.php';
 require_once '../includes/jwt_helper.php';
--- api/register.php
+++ api/register.php
@@ -3,6 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
 require_once '../config/db.php';
 
